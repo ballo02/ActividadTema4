11995e4977dc8742f107d70a6f536ecf30fefd9f
Miguel Caballo <miguel.cabbar@educa.jcyl.es>
2021-04-22 13:55:59 +0200

    Validación de linea de texto en el metodo main

--- actividad4/src/AleatorioEmple.java
+++ actividad4/src/AleatorioEmple.java
@@ -7,6 +7,7 @@ public class AleatorioEmple {
 	public static void main(String[] args) throws IOException {
 		crear();
 		leer();
+		System.out.println("Texto en el método main");
 	}
 
 	public static void leer() throws IOException {
